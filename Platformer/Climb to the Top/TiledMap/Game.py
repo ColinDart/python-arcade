364b885175c1a56f937ab661427e0ebcf9f93804
Colin Dart <41739180+ColinDart@users.noreply.github.com>
2023-03-05 18:25:11 +1100

    Buttons & more valuable coins - bronze=1, silver=2, gold =3 - button-activated green worms - button activated springs - Fix restart bug - Add restart level cheat - Add start position cheat

--- Platformer/Climb to the Top/TiledMap/Game.py
+++ Platformer/Climb to the Top/TiledMap/Game.py
@@ -6,8 +6,12 @@ from typing import Optional
 import arcade
 from arcade import SpriteList
 
-CHEATS = {"startLevel": 2,
-          'keyLocks': True}
+CHEATS = {'startLevel': 2,
+          'restart': 'level',
+          'keyLocks': False,
+          'startX': 300,
+          'startY': 500,
+          }
 
 UNLOCK_DISTANCE = 41
 
@@ -43,8 +47,8 @@ PLAYER_JUMP_SPEED = 9
 SPRING_RATIO = 1.7
 PLAYER_START_X = [85, 50]
 PLAYER_START_Y = [128, 58]
-GAME_OVER_TIMER = 100
-LEVEL_WON_TIMER = 100
+GAME_OVER_TIMER = 70
+LEVEL_WON_TIMER = 50
 
 
 class MyGame(arcade.Window):
@@ -102,7 +106,7 @@ class MyGame(arcade.Window):
     def is_level_won(self):
         return self.level_won_countdown > 0
 
-    def setup(self, level):
+    def setup(self, level=1):
         """Set up the game here. Call this function to restart the game."""
 
         self.level = level if level else 1
@@ -148,8 +152,8 @@ class MyGame(arcade.Window):
         # Set up the player, specifically placing it at these coordinates.
         src = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
         self.player_sprite = arcade.Sprite(src, CHARACTER_SCALING)
-        self.player_sprite.center_x = PLAYER_START_X[level - 1]
-        self.player_sprite.center_y = PLAYER_START_Y[level - 1]
+        self.player_sprite.center_x = CHEATS.get('startX') if CHEATS.get('startX') else PLAYER_START_X[level - 1]
+        self.player_sprite.center_y = CHEATS.get('startY') if CHEATS.get('startY') else PLAYER_START_Y[level - 1]
         self.scene.add_sprite("Player", self.player_sprite)
 
         # --- Other stuff
@@ -158,9 +162,12 @@ class MyGame(arcade.Window):
         locked_doors = self.get_layer("LockedDoors")
         if locked_doors:
             barrier_sprites.extend(locked_doors)
-        green_worms = self.get_layer("GreenWorms")
-        if green_worms:
-            barrier_sprites.extend(green_worms)
+        tall_green_worms = self.get_layer("TallGreenWorms")
+        if tall_green_worms:
+            barrier_sprites.extend(tall_green_worms)
+        short_green_worms = self.get_layer("ShortGreenWorms")
+        if short_green_worms:
+            barrier_sprites.extend(short_green_worms)
         for colour in KEY_LOCK_COLOURS:
             barrier_sprites.extend(self.get_layer(f"{colour}Lock"))
         self.physics_engine = arcade.PhysicsEnginePlatformer(
@@ -233,7 +240,7 @@ class MyGame(arcade.Window):
             case arcade.key.ESCAPE:
                 self.close()
             case arcade.key.ENTER:
-                self.setup()
+                self.setup(self.level if CHEATS.get('restart') == 'level' else 1)
             case arcade.key.DOWN:
                 # See if we're at an exit
                 exits_layer = self.get_layer("Exits")
@@ -294,6 +301,8 @@ class MyGame(arcade.Window):
 
         self.process_springs()
 
+        self.process_buttons()
+
         if self.player_sprite.center_y + (SPRITE_PIXEL_SIZE / 2) <= 0:
             self.set_game_over()
 
@@ -312,8 +321,14 @@ class MyGame(arcade.Window):
         for hit in hit_list:
             # Remove the coin
             hit.remove_from_sprite_lists()
-            # Add one to the score
-            self.score += 1
+            # Add to the score
+            match hit.properties['tile_id']:
+                case 16:  # bronze
+                    self.score += 1
+                case 18:  # silver
+                    self.score += 2
+                case 17:  # gold
+                    self.score += 3
 
     def process_keys_and_locks(self, colour):
         # See if we hit the key
@@ -358,6 +373,33 @@ class MyGame(arcade.Window):
         for _ in hit_list:
             # Spring the player
             self.player_sprite.change_y = PLAYER_JUMP_SPEED * SPRING_RATIO
+
+    def process_buttons(self):
+        # See if we hit any buttons
+        hit_list = arcade.check_for_collision_with_list(
+            self.player_sprite, self.get_layer("Buttons")
+        )
+        # Loop through each button we hit (if any)
+        for hit in hit_list:
+            hit.remove_from_sprite_lists()
+            match hit.properties['tile_id']:
+                case 5:  # green button
+                    sprites: SpriteList = self.get_layer("TallGreenWorms")
+                    for worm in sprites.sprite_list:
+                        # Assume the Tiled map has a shorter green worm in a layer behind each tall green worm.
+                        # So all we need to do is remove the taller green worm to reveal the shorter one.
+                        worm.remove_from_sprite_lists()
+                case 7:  # red button
+                    sprites: SpriteList = self.get_layer("RedSprings")
+                    for inactive_spring in sprites.sprite_list:
+                        # Replace the inactive spring with an active one
+                        inactive_spring.remove_from_sprite_lists()
+
+                        src = "../Assets/Items/springboardUp.png"
+                        active_spring = arcade.Sprite(src, TILE_SCALING)
+                        active_spring.center_x = inactive_spring.center_x
+                        active_spring.center_y = inactive_spring.center_y
+                        self.scene['Springs'].append(active_spring)
 
     def on_resize(self, width, height):
         new_width = width if width <= SCREEN_WIDTH else SCREEN_WIDTH
